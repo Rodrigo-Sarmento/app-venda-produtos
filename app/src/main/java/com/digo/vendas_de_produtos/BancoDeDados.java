5023fa5edc4f3a057078b15f94934b917a5bb5ee
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-06-09 22:40:05 -0300

    feat: buscar por nomes de clientes e produtos na venda

--- app/src/main/java/com/digo/vendas_de_produtos/BancoDeDados.java
+++ app/src/main/java/com/digo/vendas_de_produtos/BancoDeDados.java
@@ -83,6 +83,61 @@ public class BancoDeDados extends SQLiteOpenHelper {
         return produtos;
     }
 
+    @SuppressLint("Range")
+    public List<Produto> searchProdutos(String input) {
+        List<Produto> produtos = new ArrayList<>();
+
+        // Conexão com o banco de dados e consulta para obter os produtos com nomes que contenham o texto de entrada
+        String selectQuery = "SELECT * FROM TbProduto WHERE nomeProduto LIKE '%" + input + "%';";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // Percorrer o cursor e criar objetos Produto com os dados obtidos
+        if (cursor.moveToFirst()) {
+            do {
+                Produto produto = new Produto();
+                produto.set_idProduto(cursor.getInt(cursor.getColumnIndex("_idProduto")));
+                produto.setNomeProduto(cursor.getString(cursor.getColumnIndex("nomeProduto")));
+                produto.setPreco(cursor.getDouble(cursor.getColumnIndex("preco")));
+
+                produtos.add(produto);
+            } while (cursor.moveToNext());
+        }
+
+        // Fechar o cursor e a conexão com o banco de dados
+        cursor.close();
+        db.close();
+
+        return produtos;
+    }
+
+    @SuppressLint("Range")
+    public List<Cliente> searchClientes(String input) {
+        List<Cliente> clientes = new ArrayList<>();
+
+        // Conexão com o banco de dados e consulta para obter os clientes com nomes que contenham o texto de entrada
+        String selectQuery = "SELECT * FROM TbCliente WHERE nomeCliente LIKE '%" + input + "%';";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // Percorrer o cursor e criar objetos Cliente com os dados obtidos
+        if (cursor.moveToFirst()) {
+            do {
+                Cliente cliente = new Cliente();
+                cliente.set_idCliente(cursor.getInt(cursor.getColumnIndex("_idCliente")));
+                cliente.setNomeCliente(cursor.getString(cursor.getColumnIndex("nomeCliente")));
+
+                clientes.add(cliente);
+            } while (cursor.moveToNext());
+        }
+
+        // Fechar o cursor e a conexão com o banco de dados
+        cursor.close();
+        db.close();
+
+        return clientes;
+    }
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
--- app/src/main/java/com/digo/vendas_de_produtos/VendaActivity.java
+++ app/src/main/java/com/digo/vendas_de_produtos/VendaActivity.java
@@ -3,26 +3,115 @@ package com.digo.vendas_de_produtos;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VendaActivity extends AppCompatActivity {
     private ImageView ivVoltarVenda;
     private Button btnAumentarQtd;
     private Button btnDiminuirQtd;
     private EditText qtdProduto;
+    private AutoCompleteTextView nomeProduto;
+    private AutoCompleteTextView nomeCliente;
+    private BancoDeDados db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venda);
+        this.db = BancoDeDados.Sharedinstance(this);
 
         ivVoltarVenda = findViewById(R.id.imageView6);
         btnAumentarQtd = findViewById(R.id.aumentarQTD);
         btnDiminuirQtd = findViewById(R.id.diminuirQTD);
         qtdProduto = findViewById(R.id.entQtdProduto);
+        nomeProduto = findViewById(R.id.entNomeProduto);
+        nomeCliente = findViewById(R.id.txtNomeComprador);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, new ArrayList<>());
+        nomeProduto.setAdapter(adapter);
+        nomeCliente.setAdapter(adapter);
+        nomeCliente.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String input = s.toString().trim();
+
+                // Faça a busca no banco de dados apenas se o texto digitado for maior que 0
+                if (input.length() > 0) {
+                    List<Cliente> clientes = db.searchClientes(input);
+
+                    List<String> nomesClientes = new ArrayList<>();
+                    for (Cliente cliente : clientes) {
+                        nomesClientes.add(cliente.getNomeCliente());
+                    }
+
+                    // Limpe o adaptador antes de adicionar as novas sugestões
+                    adapter.clear();
+
+                    // Adicione as sugestões ao adaptador
+                    adapter.addAll(nomesClientes);
+
+                    // Notifique o adaptador de que os dados foram atualizados
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //consultando o nome dos produtos enquanto o usuário digita
+        nomeProduto.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String input = s.toString().trim();
+
+                // Faça a busca no banco de dados apenas se o texto digitado for maior que 0
+                if (input.length() > 0) {
+                    List<Produto> produtos = db.searchProdutos(input);
+
+                    List<String> nomesProdutos = new ArrayList<>();
+                    for (Produto produto : produtos) {
+                        nomesProdutos.add(produto.getNomeProduto());
+                    }
+
+                    // Limpe o adaptador antes de adicionar as novas sugestões
+                    adapter.clear();
+
+                    // Adicione as sugestões ao adaptador
+                    adapter.addAll(nomesProdutos);
+
+                    // Notifique o adaptador de que os dados foram atualizados
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
 
         ivVoltarVenda.setOnClickListener(new View.OnClickListener() {
             @Override
